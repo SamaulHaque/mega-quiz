2db1f8bca0f3c8749584bc821a323ba9ba6bf3d1
SamaulHaque <samaulhaque749@gmail.com>
2022-10-12 20:01:04 +0600

    chart make responsive

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,} from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer} from 'recharts';
 import './Statistics.css'
 
 
@@ -10,10 +10,17 @@ const Statistics = () => {
   const data = allQuiz.data;
     
     return (
-      <LineChart className='chart'
+      <ResponsiveContainer className='chart' width="95%" height={300}>
+      <LineChart 
       width={500}
       height={300}
       data={data}
+      margin={{
+        top: 20,
+        right: 20,
+        left: 20,
+        bottom: 20,
+      }}
       >
       <CartesianGrid strokeDasharray="3 3" />
       <XAxis dataKey="name" />
@@ -22,7 +29,8 @@ const Statistics = () => {
       <Legend />
       <Line type="monotone" dataKey="total" stroke="#8884d8" activeDot={{ r: 8 }} />
     </LineChart>
-   
+    </ResponsiveContainer>
+
     
     );
 };
